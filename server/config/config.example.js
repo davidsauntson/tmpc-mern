b40c996bf7e9a7ca3eb3c71a21f93146aa07254b
davidsauntson <david.sauntson@citizensadvice.org.uk>
2023-09-30 11:54:42 +0200

    initial commit

--- server/config/config.example.js
+++ server/config/config.example.js
@@ -2,7 +2,7 @@
 //'config.js' is usually ignored by git to protect sensitive information, such as your database's username and password
 
 module.exports = {
-    db: {
-        uri: '', //place the URI of your mongo database here.
-    }
+  db: {
+    uri: "", //place the URI of your mongo database here.
+  },
 };
